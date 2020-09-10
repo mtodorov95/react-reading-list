14254e6404482381133ef1cc42ab0c8dad3a1d33
mtodorov95 <mtodorov95@gmail.com>
2020-09-10 09:19:04 +0300

    book context

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from "react";
+import BookContextProvider from "./contexts/BookContext";
+import Navbar from "./components/Navbar";
 import "./App.css";
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <BookContextProvider>
+        <Navbar />
+      </BookContextProvider>
+    </div>
+  );
 }
 
 export default App;
